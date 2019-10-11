adde2b9bf9116a4bf34ae7bd2eb9d25fd3d79376
Gustavo <gustavo.tatarem@saedigital.com.br>
2019-10-10 23:08:26 -0300

    Created Landmarks XCode project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gustavo Tatarem on 23/09/19.
+//  Copyright © 2019 Gustavo Tatarem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                    .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+        
