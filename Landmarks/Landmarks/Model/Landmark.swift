37c572df91aee1478ef2fd40bacde4483793ac14
Gustavo <gustavo.tatarem@saedigital.com.br>
2019-10-10 23:33:55 -0300

    Added json data and images

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Gustavo Tatarem on 10/10/19.
+//  Copyright © 2019 Gustavo Tatarem. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
